d32ca0640208c35ee47ac6b1433dcf5a00598954
Sadique <sadiqueklr@gmail.com>
2026-04-23 20:22:04 +0800

    feat: enhance S3 bucket policy for bootstrap ignition access and update ignition location format in UPI setup

--- aws/self-managed/upi/terraform-three-az/locals.tf
+++ aws/self-managed/upi/terraform-three-az/locals.tf
@@ -5,11 +5,8 @@ locals {
   master_ignition_location = "https://api-int.${var.cluster_name}.${var.cluster_domain}:22623/config/master"
   worker_ignition_location = "https://api-int.${var.cluster_name}.${var.cluster_domain}:22623/config/worker"
 
-  # Bootstrap ignition is fetched from S3 by the bootstrap instance at boot.
-  # Uses the regional HTTPS URL so traffic routes through the S3 VPC Gateway
-  # endpoint in disconnected environments (s3:// scheme can fail if Ignition
-  # cannot determine the bucket region).
-  bootstrap_ignition_location = "https://${local.infra_id}.s3.${var.aws_region}.amazonaws.com/bootstrap.ign"
+  # Bootstrap ignition is fetched from S3 by the bootstrap instance at boot
+  bootstrap_ignition_location = "s3://${local.infra_id}/bootstrap.ign"
 
   # Instance profile names match what openshift-install expects
   master_instance_profile = "${local.infra_id}-master-profile"
